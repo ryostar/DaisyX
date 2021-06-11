bd9b33e8185b663cc21aa244b286218774b0169e
ryostar <82922436+ryostar@users.noreply.github.com>
2021-06-12 04:05:26 +0700

    Update zz_misc.py

--- DaisyX/modules/zz_misc.py
+++ DaisyX/modules/zz_misc.py
@@ -1,62 +1,62 @@
-__mod_name__ = "Misc ⚙️"
+__mod_name__ = "Linh tinh ⚙️"
 
 __help__ = """
-An "odds and ends" module for small, simple commands which don't really fit anywhere.
+Một mô-đun "tỷ lệ cược và kết thúc" cho các lệnh nhỏ, đơn giản không thực sự phù hợp với bất kỳ đâu..
 
-<b>Available commands:</b>
+<b>Các lệnh có sẵn:</b>
 
-<b>BASIC</b>
-- /github (username): Returns info about a GitHub user or organization.
-- /wiki (keywords): Get wikipedia articles just using this bot.
-- /imdb: Search for a movie
-- /cancel: Disables current state. Can help in cases if DaisyXBot not responing on your message.
-- /id: get the current group id. If used by replying to a message, gets that user's id.
-- /info: get information about a user.
+<b>CƠ BẢN</b>
+- /github (username):Trả về thông tin về người dùng hoặc tổ chức GitHub.
+- /wiki (keywords): Nhận các bài báo wikipedia chỉ bằng cách sử dụng bot này..
+- /imdb: Tìm kiếm một bộ phim
+- /cancel: Tắt trạng thái hiện tại. Có thể trợ giúp trong trường hợp bot không phản hồi trong tin nhắn của bạn.
+- /id: lấy id nhóm hiện tại.  Nếu được sử dụng bằng cách trả lời một tin nhắn, hãy lấy id của người dùng đó.
+- /info: nhận thông tin về một người dùng.
 - /paste: Pase the text/file in nekobin
-- /gps: Find a location
+- /gps: Tìm kiếm vị trí
 
 <b>BOOK DOWNLOAD </b>
-- /book <i>book name</i> : Usage :Gets Instant Download Link Of Given Book.
+- /book <i>book name</i> : Cách sử dụng: Nhận liên kết tải xuống tức thì của cuốn sách đã cho.
 
 <b>FAKE INFO</b>
-- /fakegen : Generates Fake Information
-- /picgen : generate a fake pic
+- /fakegen : Tạo thông tin giả mạo
+- /picgen : tạo một pic giả
 
 <b>ZIPPER</b>
-- /zip: reply to a telegram file to compress it in .zip format
-- /unzip: reply to a telegram file to decompress it from the .zip format
+- /zip: trả lời một tệp điện tín để nén nó ở định dạng .zip
+- /unzip: trả lời một tệp điện tín để giải nén nó từ định dạng .zip
 
 <b>WEATHER</b>
-- /weather: Gives weather forcast
-- /wheatherimg: Gives weather image
+- /weather: Cung cấp dự báo thời tiết
+- /wheatherimg: Cung cấp hình ảnh thời tiết
 
 <b>PHONE INFO</b>
-- /phone [phone no]: Gathers no info
+- /phone [phone no]: Thu thập không có thông tin
 
-<b>CURRENCY</b>
- - /cash : currency converter
+<b>TIỀN TỆ</b>
+ - /cash: Chuyển đổi tiền tệ
 Example syntax: `/cash 1 USD INR`
 
 <b>NAME HISTORY</b>
-- /namehistory [REPLY]: Get the Username and Name history of user.
+- /namehistory [REPLY]: Lấy tên người dùng và lịch sử tên của người dùng.
 
 <b>SEND</b>
-- /send [MESSAGE]: Send given text by bot.
+- /send [MESSAGE]: Gửi văn bản nhất định bằng bot
 
 <b>CC CHECKER</b>
-- /au [cc]: Stripe Auth given CC
-- /pp [cc]: Paypal 1$ Guest Charge
+- /au [cc]: Sọc Auth được cấp CC
+- /pp [cc]: Paypal 1 $ Phí khách
 - /ss [cc]: Speedy Stripe Auth
-- /ch [cc]: Check If CC is Live
-- /bin [bin]: Gather's Info About the bin
-- /gen [bin]: Generates CC with given bin
-- /key [sk]: Checks if Stripe key is Live
+- /ch [cc]: Kiểm tra xem CC có Trực tiếp không
+- /bin [bin]: Thu thập thông tin về thùng
+- /gen [bin]: Tạo CC với thùng đã cho
+- /key [sk]: Kiểm tra xem phím Stripe có Trực tiếp hay không
 
-  Note: Format of cc is ccnum|mm|yy|cvv
-  Privacy warning: Don't check any of your personal CC's.
-  
+   Lưu ý: Định dạng của cc là ccnum|mm|yy|cvv
+   Cảnh báo về quyền riêng tư: Không kiểm tra bất kỳ CC cá nhân nào của bạn.
+
 <b>URL TOOLS</b>
-- /short (url): Shortify given url.
-- /ip (url): Displays information about an IP / domain.
-- /direct (url): Generates direct links from the sourceforge.net
+- /short (url): Rút gọn url đã cho.
+- /ip (url): Hiển thị thông tin về IP / miền.
+- /direct (url): Tạo liên kết trực tiếp từ sourceforge.net
 """
